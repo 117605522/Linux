1ecc30225a26ace15a8dcf40c630f358876683ac
DELL <wang_.chao@qq.com>
2019-01-25 16:53:24 +0800

    取消了红外，但是可以通过配置文件打开 修改刷卡不上报问题

---
+++ 2_红外人脸 界面已改 20190109/xboard.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export DISPLAY=:0.0
+export QT_GSTREAMER_CAMERA_ROTATION=left
+export QT_GSTREAMER_CAMERABIN_VIDEOSRC=imxv4l2src
+export QT_GSTREAMER_WIDGET_VIDEOSINK=overlaysink
+#export QT_GSTREAMER_OVERLAYSINK_ROTATION=left
+export LANG=zh_CN.UTF-8
+if [ ! -d "$myPath"]; then
+	mkdir /ramfs
+	mount -t ramfs none /ramfs
+fi
+while true
+do
+	/home/root/xboard
+	sleep 1
+done
